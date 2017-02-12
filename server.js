ba1965b5ee9748d9ee1d1cc4fb1f48b08e99b5a3
wan-atharva7 <wan.atharva7@gmail.com>
2017-02-12 18:34:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one',function(req, res){
+    res.send('Article one was requested and will be sent here');
+});
+
+app.get('article-two',function(req, res){
+    res.send('Article two was requested and will be sent here');
+});
+
+app.get('article-three',function(req, res){
+    res.send('Article three was requested and will be sent here');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
