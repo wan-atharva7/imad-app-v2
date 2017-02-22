74e1960b2cbf3ff0dea5b473a6914f4d6951f027
wan-atharva7 <wan.atharva7@gmail.com>
2017-02-22 21:55:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ submit.onclick=function(){
     var names= ['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<names.lenght;i++){
-      list= 'li'+names[i]+'li';  
+      list= '<li>'+names[i]+'</li>';  
     }
     var ul = document.getElementById('ulist');
     ul.innerHTML = list;
