28eea019600c2970cd8bed91cb783492a95d72b1
wan-atharva7 <wan.atharva7@gmail.com>
2017-02-22 21:28:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,13 @@
 var button = document.getElementById('counter');
 
 button.onclick = function(){
+//create request
 var request= new XMLHttpRequest();
+//
 request.onreadystatechange=function(){
   if(request.readyState == XMLHttpRequest.DONE)
   {
+      //take some action
       if(request.status==200)
       {
             var counter = request.responseText;
@@ -13,6 +16,7 @@ request.onreadystatechange=function(){
       }
   }
 };
+//make request
 request.open('GET','http://wan-atharva7.imad.hasura-app.io/counter',true);
 request.send(null);
 };
