da088a3ebd1cd2c229d618be72b9ce733c1cd275
wan-atharva7 <wan.atharva7@gmail.com>
2017-02-22 16:15:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var img =  document.getElementById('madi');
+var marginLeft =0;
+function moveRight(){
+    marginLeft+=10;
+    img.style.marginLeft= marginLeft+'px';
+}
 img.onclick= function(){
-    img.style.marginLeft='100px';
+    var interval= setInterval(moveRight,100);
 };
