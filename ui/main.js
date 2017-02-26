00f1a15e49e2742d60a139fc0be8e9b36ca77cee
wan-atharva7 <wan.atharva7@gmail.com>
2017-02-26 18:45:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ request.send(null);
 //change the contents of a list
 
 var nameInput = document.getElementById('name');
-var name = nameInput.value;
+var nameN = nameInput.value;
 var submit = document.getElementById('btn');
 submit.onclick=function(){
     var names= ['name1','name2','name3','name4'];
